90e68a38b25bbf49d3eb09d7e79845057ea57078
Mitchell Jepsergon Ngetich <mitchell.ngetich@student.moringaschool.com>
2022-10-28 11:00:36 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+-- INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "Brown", "Neutral", True);
+-- INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES ("Rowdy", 4, "M", "Green", "Neutral", True);
+-- INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES ("Tabitha", 1, "M", "Orange", "High", True);
+-- INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES ("Sergeant Brown", 5, "F", "Red", "Low", True);
+-- INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES ("Melissa", 8, "M", "Purple", "Moderate", False);
+-- INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES ("Grinch", 3, "M", "Blue", "Low", True);
+-- INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES ("Wendy", 7, "F", "White", "Neutral", True);
+-- INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES (null, 1, "M", "Black", "High", False);
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 10, "M", "black", "intense", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "dark brown", "Nice", 0);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 19, "M", "Green", "Slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 13, "F", "dark brown", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Black", "Grinchy", 1);
+INSERT INTO bears VALUES (7,"Wendy", 12, "F", "Blue", "naive", 0);
+INSERT INTO bears VALUES (8,null, 20, "M", "black", "aggressive", 0);
+
+
+
+
+
+
+
