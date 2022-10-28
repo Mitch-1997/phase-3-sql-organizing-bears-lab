5a063a7654b17d6dd6d7b88a25a5552dfefa6d43
Mitchell Jepsergon Ngetich <mitchell.ngetich@student.moringaschool.com>
2022-10-28 15:18:55 +0300

    Completed assignment

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,3 @@
--- INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "Brown", "Neutral", True);
--- INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES ("Rowdy", 4, "M", "Green", "Neutral", True);
--- INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES ("Tabitha", 1, "M", "Orange", "High", True);
--- INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES ("Sergeant Brown", 5, "F", "Red", "Low", True);
--- INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES ("Melissa", 8, "M", "Purple", "Moderate", False);
--- INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES ("Grinch", 3, "M", "Blue", "Low", True);
--- INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES ("Wendy", 7, "F", "White", "Neutral", True);
--- INSERT INTO bears (name, age,sex,color,temperament, alive) VALUES (null, 1, "M", "Black", "High", False);
 INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
 INSERT INTO bears VALUES (2,"Rowdy", 10, "M", "black", "intense", 1);
 INSERT INTO bears VALUES (3,"Tabitha", 6, "F", "dark brown", "Nice", 0);
